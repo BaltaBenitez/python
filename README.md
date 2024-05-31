3f9f94fc6dea39b5c079c5a8311583e191a525cf
baltasar benitez <baltaarex363@gmail.com>
2024-05-30 23:28:27 -0300

    readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# PY 
+
+```python
+print('Hello World') 
+``` 
--- variables1.py
+++
@@ -1,5 +0,0 @@
-""" edad = input("ingrese su edad")
-validar = int(edad) """
-""" texto1 = "HOLA MUNDO"
-texto2 = ", Soy baltii"
-print(texto1 +  "\n" + texto2.upper()) """
